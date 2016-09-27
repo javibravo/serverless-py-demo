295136eff348517299313eff0381f2c09c910ac6
Javier Bravo <javibravo85@gmail.com>
2016-09-27 12:09:34 +0100

    Function to send message to SQS queue

--- handler.py
+++ handler.py
@@ -1,2 +1,16 @@
+import boto3
+import json
+
+
 def hello(event, context):
     return { "message": "Go Serverless v1.0! Your function executed successfully!", "event": event }
+
+
+def push_to_sqs(event, context):
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='serverless-demo')
+
+    message_body = {'from': 'Javier Bravo', 'text': 'message text', 'to': {'emails': ['bob@email.com', 'mike@email.com']}}
+    response = queue.send_message(MessageBody=json.dumps(message_body))
+
+    return response
